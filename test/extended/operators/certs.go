a56bd6e96c9fa3c184dc52333c83539df305a43e
Paul Bastide <pbastide@redhat.com>
2025-10-20 10:44:36 -0400

    fix: push the cli creation into BeforeEach, trying to re-order to avoid issues with Ginkgo
    Signed-off-by: Paul Bastide <pbastide@redhat.com>
    Assited-by: Bob

--- test/extended/operators/certs.go
+++ test/extended/operators/certs.go
@@ -91,11 +91,12 @@ func gatherCertsFromPlatformNamespaces(ctx context.Context, kubeClient kubernete
 var _ = g.Describe(fmt.Sprintf("[sig-arch][Late][Jira:%q]", "kube-apiserver"), g.Ordered, func() {
 	defer g.GinkgoRecover()
 
-	oc := exutil.NewCLIWithoutNamespace("certificate-checker")
+	var oc *exutil.CLI
 	ctx := context.Background()
 
 	g.BeforeAll(func() {
 		ctx := context.Background()
+		oc := exutil.NewCLIWithoutNamespace("certificate-checker")
 		kubeClient := oc.AdminKubeClient()
 		if ok, _ := exutil.IsMicroShiftCluster(kubeClient); ok {
 			g.Skip("microshift does not auto-collect TLS.")
